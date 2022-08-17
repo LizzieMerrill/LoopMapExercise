5fbabfc4aceed0a97b0518a73b146ead8ee087b0
LizzieMerrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-08-17 15:59:41 -0700

    Update HashMapExercise.java
    Worked on all methods

--- HashMapExercise.java
+++ HashMapExercise.java
@@ -22,11 +22,82 @@ class HashMapExercise{
 	  makeByModel.put("Grand Cherokee", "Jeep");
 	  
 	  
+	  introDialogue();
+	  
+	  //recursive method that prompts user until they select a valid option to browse, add or quit
+	public static void introDialogue(){
+		
 	  System.out.print("Hello valued customer! Would you like to browse our current selection
-			   of cars, or woud you like to request a new car to be added to our
-			   dealership? (Answer 'b' to browse or 'a' to add new car): ");
+			   of cars, or would you like to request a new car be added to our dealership?
+			   (Answer 'b' to browse, 'a' to add new car or 'q' to quit): ");
+			   
 	  char customerChoice = scnr.next();
 	  
+	  switch(customerChoice){
+		  case b:
+		  	browseCars();
+		  break;
+		  case a:
+		  	addCar();
+		  break;
+		  case q:
+		 	 System.out.println("Thank you, come again!");
+		  break;
+		  default:
+		  	System.out.println("Whoops, we didn't understand that! Please try again!");
+		  	introDialogue();//repeats dialogue if user fails to select valid option
+		  break;
+	  }
+			   
+	}//end of introDialogue method
+			   
+			   
+	public static void browseCars(){
+		
+		System.out.println("Do you have a specific car model in mind,
+				   or would you like to see a list of what cars
+				   are in stock right now? (Answer 'y' is you have
+				   a model in mind, 'n' if you would like to 
+				   list your options, or 'q' to quit): ");
+				   
+		char customerChoice2 = scnr.next();
+				   
+		switch(customerChoice2){
+		  case y:
+		  	System.out.println("What model is the car you're looking for? ");
+			String carModel = scnr.nextLine();
+			if(){
+				System.out.printf("Oh, you're looking for a %s? Our %s selection is right over here...\n );
+			}
+			else{
+				System.out.printf"We're terribly sorry, but we do not have %s model cars in stock right now.
+						   Would you like to add them to our dealership? (Answer 'y' to add, 'b' to
+						   browse again, 
+			}
+		  break;
+		  case n:
+		  	listSelection();
+			System.out.print("Now, knowing this, ");
+			browseCars();
+		  break;
+		  case q:
+		 	 System.out.println("Thank you, come again!");
+		  break;
+		  default:
+		  	System.out.println("Whoops, we didn't understand that! Please try again!");
+		  	browseCars();//repeats dialogue if user fails to select valid option
+		  break;
+	  }
+	}//end of browseCars method
+			   
+			   
+	public static void addCar(){
+		
+	}//end of addCar method
+			   
+	public static void listSelection(){
+	
+	}//end of listSelection method
   }
 }
 /*Hash Map Tutorial Code
